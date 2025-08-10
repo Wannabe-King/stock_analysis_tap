95179553676bcff0cbb436741316cb6b550be9a7
Priyanshu <sidarsunita2@gmail.com>
2025-08-11 03:54:33 +0530

    bonddetail bloc event

--- lib/features/bonddetail/ui/bloc/bonddetail_bloc.dart
+++ lib/features/bonddetail/ui/bloc/bonddetail_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
@@ -10,8 +12,22 @@ part 'bonddetail_state.dart';
 @injectable
 class BonddetailBloc extends Bloc<BonddetailEvent, BonddetailState> {
   BonddetailBloc() : super(BonddetailState.initial()) {
-    on<BonddetailEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<AnalysisTabEvent>(analysisTabEvent);
+    on<ProsConsTabEvent>(prosConsTabEvent);
+    on<EbitdaChartTabEvent>(ebitdaChartTabEvent);
+    on<RevenueChartTabEvent>(revenueChartTabEvent);
+  }
+
+  FutureOr<void> analysisTabEvent(AnalysisTabEvent event, Emitter<BonddetailState> emit) {
+    
+  }
+
+  FutureOr<void> prosConsTabEvent(ProsConsTabEvent event, Emitter<BonddetailState> emit) {
+  }
+
+  FutureOr<void> ebitdaChartTabEvent(EbitdaChartTabEvent event, Emitter<BonddetailState> emit) {
+  }
+
+  FutureOr<void> revenueChartTabEvent(RevenueChartTabEvent event, Emitter<BonddetailState> emit) {
   }
 }
