6fbd072f984ec47cfa2e3844ad0d8b2f20b06af4
Priyanshu <sidarsunita2@gmail.com>
2025-08-10 12:35:44 +0530

    models created and config generated

---
+++ lib/domain/bond_details/bond_detail_model.g.dart
@@ -0,0 +1,91 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bond_detail_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_BondDetailModel _$BondDetailModelFromJson(Map<String, dynamic> json) =>
+    _BondDetailModel(
+      logo: json['logo'] as String,
+      companyName: json['company_name'] as String,
+      description: json['description'] as String,
+      isin: json['isin'] as String,
+      status: json['status'] as String,
+      prosAndCons: ProsAndCons.fromJson(
+        json['pros_and_cons'] as Map<String, dynamic>,
+      ),
+      financials: Financials.fromJson(
+        json['financials'] as Map<String, dynamic>,
+      ),
+      issuerDetails: IssuerDetails.fromJson(
+        json['issuer_details'] as Map<String, dynamic>,
+      ),
+    );
+
+Map<String, dynamic> _$BondDetailModelToJson(_BondDetailModel instance) =>
+    <String, dynamic>{
+      'logo': instance.logo,
+      'company_name': instance.companyName,
+      'description': instance.description,
+      'isin': instance.isin,
+      'status': instance.status,
+      'pros_and_cons': instance.prosAndCons,
+      'financials': instance.financials,
+      'issuer_details': instance.issuerDetails,
+    };
+
+_ProsAndCons _$ProsAndConsFromJson(Map<String, dynamic> json) => _ProsAndCons(
+  pros: (json['pros'] as List<dynamic>).map((e) => e as String).toList(),
+  cons: (json['cons'] as List<dynamic>).map((e) => e as String).toList(),
+);
+
+Map<String, dynamic> _$ProsAndConsToJson(_ProsAndCons instance) =>
+    <String, dynamic>{'pros': instance.pros, 'cons': instance.cons};
+
+_Financials _$FinancialsFromJson(Map<String, dynamic> json) => _Financials(
+  ebitda: (json['ebitda'] as List<dynamic>)
+      .map((e) => MonthValue.fromJson(e as Map<String, dynamic>))
+      .toList(),
+  revenue: (json['revenue'] as List<dynamic>)
+      .map((e) => MonthValue.fromJson(e as Map<String, dynamic>))
+      .toList(),
+);
+
+Map<String, dynamic> _$FinancialsToJson(_Financials instance) =>
+    <String, dynamic>{'ebitda': instance.ebitda, 'revenue': instance.revenue};
+
+_MonthValue _$MonthValueFromJson(Map<String, dynamic> json) => _MonthValue(
+  month: json['month'] as String,
+  value: (json['value'] as num).toInt(),
+);
+
+Map<String, dynamic> _$MonthValueToJson(_MonthValue instance) =>
+    <String, dynamic>{'month': instance.month, 'value': instance.value};
+
+_IssuerDetails _$IssuerDetailsFromJson(Map<String, dynamic> json) =>
+    _IssuerDetails(
+      issuerName: json['issuer_name'] as String,
+      typeOfIssuer: json['type_of_issuer'] as String,
+      sector: json['sector'] as String,
+      industry: json['industry'] as String,
+      issuerNature: json['issuer_nature'] as String,
+      cin: json['cin'] as String,
+      leadManager: json['lead_manager'] as String,
+      registrar: json['registrar'] as String,
+      debentureTrustee: json['debenture_trustee'] as String,
+    );
+
+Map<String, dynamic> _$IssuerDetailsToJson(_IssuerDetails instance) =>
+    <String, dynamic>{
+      'issuer_name': instance.issuerName,
+      'type_of_issuer': instance.typeOfIssuer,
+      'sector': instance.sector,
+      'industry': instance.industry,
+      'issuer_nature': instance.issuerNature,
+      'cin': instance.cin,
+      'lead_manager': instance.leadManager,
+      'registrar': instance.registrar,
+      'debenture_trustee': instance.debentureTrustee,
+    };
---
+++ lib/features/bonddetail/ui/bloc/bonddetail_state.dart
@@ -0,0 +1,6 @@
+part of 'bonddetail_bloc.dart';
+
+@immutable
+sealed class BonddetailState {}
+
+final class BonddetailInitial extends BonddetailState {}
